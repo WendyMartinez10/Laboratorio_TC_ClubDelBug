5bdc98ce44bac6e0035798ce7e057441026df007
WENDY_MARTINEZ <wemartinezlo@est.utn.ac.cr>
2025-07-01 16:02:08 -0600

    se crea la interfase
    La interfase list para el Agregar, Eliminar y Buscar

---
+++ src/Lists/List.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author USER
+ */
+public interface List<T>  {
+   public  boolean add(T element);
+   public boolean remove(T element);
+   public T find(Object key);
+}
